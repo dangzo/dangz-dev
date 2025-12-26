b109397eb5866993cd1659626357ae89d238389a
Daniele Gazzelloni <dangz.dev@gmail.com>
2025-12-26 09:49:48 +0000

    chore: lint

--- blog/eslint.config.mjs
+++ blog/eslint.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from 'typescript-eslint'
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
@@ -12,7 +18,46 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "dist",
   ]),
-]);
-
-export default eslintConfig;
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      //
+      'object-curly-newline': 'warn',
+      'object-curly-spacing': ['warn', 'always'],
+      'no-multi-spaces': 'error',
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+            "warn",
+            {
+                "vars": "all",
+                "varsIgnorePattern": "^_",
+                "args": "after-used",
+                "argsIgnorePattern": "^_",
+            },
+        ],
+      'no-console': 'warn',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+    },
+  },
+])
--- blog/src/sanity/client.ts
+++ blog/src/sanity/client.ts
@@ -1,8 +1,8 @@
-import { createClient } from "next-sanity";
+import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: "wdxhl3tc",
-  dataset: "production",
-  apiVersion: "2024-01-01",
+  projectId: 'wdxhl3tc',
+  dataset: 'production',
+  apiVersion: '2024-01-01',
   useCdn: false,
 });
