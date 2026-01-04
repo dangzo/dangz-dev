30f1340b5a4acb71abc0aaf81b5e7ed9790c722f
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-01-04 10:58:26 +0000

    fix: issue with GROQ query not properly called in home page

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,27 +1,11 @@
-import { Link, Text, Heading } from '@/components/ui';
-import { type SanityDocument } from 'next-sanity';
-import { POST_LIST_QUERY } from '@/api/queries';
-import { client } from '@/sanity/client';
-
-
-const options = { next: { revalidate: 30 } };
+import { Text, Link, Heading } from '@/components/ui';
 
 export default async function HomePage() {
-  const posts = await client.fetch<SanityDocument[]>(POST_LIST_QUERY, {}, options);
 
   return (
     <article>
-      <Heading as="h1" className="mb-8">Posts</Heading>
-      <ul className="flex flex-col gap-y-4">
-        {posts.map((post) => (
-          <li className="hover:underline" key={post._id}>
-            <Link href={`/${post.slug.current}`}>
-              <Heading as="h2" className="text-xl font-semibold">{post.title}</Heading>
-              <Text>{new Date(post.publishedAt).toLocaleDateString()}</Text>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Heading as="h1" className="mb-8">Home</Heading>
+      <Text>Empty. See <Link href="/blog">Blog page</Link> first.</Text>
     </article>
   );
 }
