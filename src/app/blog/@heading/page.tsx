f52353925a3001a1f6ac4cb56afe82624319e0d8
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-01-09 17:42:59 +0000

    feat: simplified blog layout by removing cards style

--- src/app/blog/@heading/page.tsx
+++ src/app/blog/@heading/page.tsx
@@ -6,7 +6,7 @@ export default function HeadingDefault() {
       <Heading as="h1" className="text-6xl font-bold mb-8">
         All Posts
       </Heading>
-      <Text className="mb-12" size="large">
+      <Text size="large">
         Brief description of what I'm writing about in my blog
       </Text>
     </>
--- src/app/blog/@heading/tags/[slug]/page.tsx
+++ src/app/blog/@heading/tags/[slug]/page.tsx
@@ -8,7 +8,7 @@ export default async function BlogTagsSlugHeading({ params }: { params: Promise<
       <Heading as="h1" className="text-6xl font-bold mb-8">
         {`Tag: ${startCase(slug)}`}
       </Heading>
-      <Text className="mb-12" size="large">
+      <Text size="large">
         Brief description of what I'm writing about in my blog
       </Text>
     </>
