589972ee5bb35335674c30c8fdacba5902ba352b
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-01-15 12:55:02 +0000

    feat: display post title

--- src/app/blog/@heading/page.tsx
+++ src/app/blog/@heading/page.tsx
@@ -3,10 +3,10 @@ import { Heading, Text } from '@/components/ui';
 export default function HeadingDefault() {
   return (
     <>
-      <Heading as="h1" className="text-6xl font-bold mb-8">
+      <Heading as="h1">
         All Posts
       </Heading>
-      <Text size="large">
+      <Text>
         Brief description of what I'm writing about in my blog
       </Text>
     </>
