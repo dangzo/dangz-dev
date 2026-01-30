b271f7a42396a220b58add34c5724523adf5a5fb
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-01-30 10:54:39 +0000

    feat: lazy loading post cards

--- src/components/blog/PostCard.tsx
+++ src/components/blog/PostCard.tsx
@@ -1,42 +1,56 @@
 import { DateText, Text, Heading, Link } from '@/components/ui';
 import { TagList } from '@/components';
+import Skeleton from 'react-loading-skeleton';
 import type { PostWithTags } from '@/types/Post.types';
 
-const PostCard = ({ post }: { post: PostWithTags }) => {
+export const PostCardSkeleton = () => {
   return (
-    <>
-      <li className="relative pb-4 p-4 duration-300  dark:bg-background-secondary-darks">
-        <div className="flex flex-row relative">
-          <DateText date={post.publishedAt} className="leading-8 min-w-38" />
-          <div>
-            <Link
-              href={`/blog/${post.slug?.current}`}
-              className="text-2xl font-semibold"
-            >
-              <Heading as="h4" className="inline-block">{post.title}</Heading>
-            </Link>
-
-            <TagList className="mt-1" tags={post.tags} />
-
-            <Text className="my-4">
-              {post.body && post.body.length > 0
-                ? post.body
-                  .filter((block) => block._type === 'block')
-                  .map((block) => block.children?.map((child) => child.text).join(' '))
-                  .join(' ')
-                  .slice(0, 150) + '...'
-                : 'No description available.'}
-
-            </Text>
-
-            <Link href={`/blog/${post.slug?.current}`} type="accent">
-              Read more →
-            </Link>
-          </div>
-        </div>
-      </li>
-    </>
+    <div className="flex flex-row relative">
+      <div className="leading-6 min-w-38">
+        <Skeleton width={120} height={20} />
+      </div>
+      <div className="flex-1">
+        <Skeleton width="60%" height={30} className="mb-2" />
+        <Skeleton width="40%" height={20} className="mb-2" />
+        <Skeleton count={3} className="mb-2" />
+        <Skeleton width={100} height={20} />
+      </div>
+    </div>
   );
 };
 
-export default PostCard;
+export const PostCard = async ({ post }: { post: PostWithTags }) => {
+  return (
+    <div className="flex flex-row relative">
+      <DateText date={post.publishedAt} className="leading-8 min-w-38" />
+      <div>
+        <Link
+          href={`/blog/${post.slug?.current}`}
+          className="text-2xl font-semibold"
+        >
+          <Heading as="h4" className="inline-block">
+            {post.title}
+          </Heading>
+        </Link>
+
+        <TagList className="mt-1" tags={post.tags} />
+
+        <Text className="my-4">
+          {post.body && post.body.length > 0
+            ? post.body
+                .filter((block) => block._type === 'block')
+                .map((block) =>
+                  block.children?.map((child) => child.text).join(' '),
+                )
+                .join(' ')
+                .slice(0, 150) + '...'
+            : 'No description available.'}
+        </Text>
+
+        <Link href={`/blog/${post.slug?.current}`} type="accent">
+          Read more →
+        </Link>
+      </div>
+    </div>
+  );
+};
