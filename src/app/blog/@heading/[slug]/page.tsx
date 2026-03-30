402e94c9c4cc32b47274cc78758fbd67fc8c51d1
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-03-30 13:00:20 +0100

    feat: display tags below post title in post page

--- src/app/blog/@heading/[slug]/page.tsx
+++ src/app/blog/@heading/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import { DateText, Heading } from '@/components/core';
 import { notFound } from 'next/navigation';
 import { getPostBySlug } from '@/api/queries/posts';
-import type { Post } from '@/types/sanity.types';
+import type { PostWithTags } from '@/types/Post.types';
+import TagList from '@/components/post-list/TagList';
 
 export default async function BlogSlugHeading({ params }: { params: Promise<{ slug?: string }> }) {
   const { slug } = await params;
@@ -10,7 +11,7 @@ export default async function BlogSlugHeading({ params }: { params: Promise<{ sl
     return notFound();
   }
 
-  const post = await getPostBySlug(slug) as Post | null;
+  const post = await getPostBySlug(slug) as PostWithTags | null;
 
   if (!post) {
     return notFound();
@@ -21,7 +22,12 @@ export default async function BlogSlugHeading({ params }: { params: Promise<{ sl
       <Heading as="h1">
         {post.title || '(Untitled)'}
       </Heading>
-      <DateText date={post.publishedAt || ''} />
+      
+      <div className="flex flex-row items-center">
+        <DateText date={post.publishedAt} className="mb-0!" />
+        <span className="mx-2">&bull;</span>
+        <TagList tags={post.tags} />
+      </div>
     </>
   );
 }
