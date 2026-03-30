ce4e994862dcda6a536082d87e54191840469ccd
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-03-30 14:35:01 +0100

    fix: improve Heading component style of h3 and h4
    Downsized h3 text size so that the difference between h2 and h3
    is more prominent.

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -11,9 +11,9 @@ const Heading = async ({ as: Tag = 'h1', children, className, ...rest }: Heading
     {
       'text-5xl font-bold mb-4': Tag === 'h1',
       'text-4xl font-semibold mb-6': Tag === 'h2',
-      'text-3xl font-semibold mb-4': Tag === 'h3',
-      'text-2xl font-semibold mb-3': Tag === 'h4',
-      'text-xl font-semibold mb-2': Tag === 'h5',
+      'text-2xl font-semibold mb-3': Tag === 'h3',
+      'text-xl font-semibold mb-3': Tag === 'h4',
+      'text-lg font-semibold mb-2': Tag === 'h5',
       'text-md font-semibold mb-1': Tag === 'h6',
     },
     className,
