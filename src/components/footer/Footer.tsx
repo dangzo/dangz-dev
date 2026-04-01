9f2733cc4897754b1ac7e88e1021875e1776f562
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-04-01 15:24:02 +0100

    feat: add custom icons to all skills

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,14 @@
 import siteMetadata from '@/data/siteMetadata';
-import SocialIcon from '@/components/social-icons';
+import Icon from '@/components/icons/Icon';
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 mb-4 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
-          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+          <Icon icon="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+          <Icon icon="github" href={siteMetadata.github} size={6} />
+          <Icon icon="linkedin" href={siteMetadata.linkedin} size={6} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{`© ${new Date().getFullYear()}`}</div>
