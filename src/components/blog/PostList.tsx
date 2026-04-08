7dba419a1fbe852137df9a4176fe615588f14d69
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-04-08 17:14:37 +0100

    chore: improve post list responsiveness

--- src/components/blog/PostList.tsx
+++ src/components/blog/PostList.tsx
@@ -26,7 +26,10 @@ export const PostList = async ({ tag, posts }: PostListProps) => {
       {filteredPosts?.map((post) => (
         <li
           key={post._id}
-          className="relative px-0 my-3 sm:mb-3 sm:mt-0 py-2 md:py-4 md:px-4 duration-300 dark:bg-background-secondary-darks border-b"
+          className="
+            relative dark:bg-background-secondary-darks border-b
+            px-0 py-6 my-3 sm:mb-3 md:py-4 md:mt-0 md:px-4
+          "
         >
           <PostCard key={post._id} post={post} />
         </li>
@@ -41,7 +44,9 @@ export const PostListSkeleton = () => {
       {Array.from({ length: 3 }).map((_, index) => (
         <li
           key={index}
-          className="relative px-0 my-3 sm:mb-3 sm:mt-0 py-2 md:py-4 md:px-4 duration-300 dark:bg-background-secondary-darks border-b"
+          className="
+            relative px-0 my-3 sm:mb-3 sm:mt-0 py-2 md:py-4 md:px-4 duration-300 dark:bg-background-secondary-darks border-b
+          "
         >
           <PostCardSkeleton />
         </li>
