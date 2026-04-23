6c01cd19d15d18e29024395e8defe9347c7a5166
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-04-23 16:12:39 +0100

    feat: improve table look & feel

--- src/components/ui/Table.tsx
+++ src/components/ui/Table.tsx
@@ -18,14 +18,24 @@ const Table = ({ value }: TableProps) => {
   
   return (
     <div className="overflow-x-auto my-8">
-      <table className="min-w-full border-collapse border border-gray-300">
+      <table
+        className="
+          min-w-full border-collapse border 
+          border-gray-300 bg-neutral-50/50 
+          dark:border-gray-700 dark:bg-neutral-800/10
+        "
+      >
         <tbody>
           {rows.map((row: TableRow, rowIndex: number) => (
             <tr key={row._key || rowIndex}>
               {row.cells.map((cell: string, cellIndex: number) => (
                 <td
                   key={cellIndex}
-                  className="border border-gray-300 p-2! align-top [&_p]:my-0 [&_ul]:my-0 [&_ul]:pl-5 [&_ol]:my-0 [&_ol]:pl-5 [&_code]:rounded [&_code]:bg-accent-light/10 [&_code]:px-1 [&_code]:py-0.5 dark:[&_code]:bg-neutral-100/10"
+                  className="
+                    border border-gray-300 dark:border-gray-700
+                    text-secondary-light dark:text-secondary-dark font-body
+                    p-2! align-top [&_p]:my-0 [&_ul]:my-0 [&_ul]:pl-5 [&_ol]:my-0 [&_ol]:pl-5 [&_code]:rounded [&_code]:bg-accent-light/10 [&_code]:px-1 [&_code]:py-0.5 dark:[&_code]:bg-neutral-100/10
+                  "
                 >
                   <ReactMarkdown remarkPlugins={[remarkGfm]}>
                     {cell}
