1ae089f6a016410ab3b2144841c2dec891bf6592
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-05-11 20:02:42 +0100

    style: make so inline code does not wrap

--- src/features/blog/components/portable-text/MarkInlineCode.tsx
+++ src/features/blog/components/portable-text/MarkInlineCode.tsx
@@ -8,6 +8,7 @@ export default function MarkInlineCode({ children }: MarkInlineCodeProps) {
   return (
     <code
       className="
+        text-nowrap
         rounded px-1 py-0.5 text-[0.8em]!
         bg-accent-light/10 dark:bg-neutral-100/10
       "
