280e92f51a86c85a237b3b9e82198c734de907e9
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-05-12 10:20:33 +0100

    chore: add semver displaying in footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { author } from '@/data/siteMetadata';
+import { BUILD_SEMVER, BUILD_YEAR } from '@/data/buildVersion';
 import SocialIcons from './SocialIcons';
 
 export default async function Footer() {
@@ -7,9 +8,11 @@ export default async function Footer() {
       <div className="mt-8 pt-8 border-t sm:border-0 mb-4 sm:mb-0 flex flex-col items-center">
         <SocialIcons />
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{' • '}</div>
+          <div>{`© ${BUILD_YEAR}`}</div>
+          <div>&bull;</div>
           <div>{author}</div>
+          <div>&bull;</div>
+          <div>v{BUILD_SEMVER}</div>
         </div>
       </div>
     </footer>
