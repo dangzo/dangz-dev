d4f11e792f0a69f0584de3b7720a66ce2ca0ec41
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-05-12 10:37:46 +0100

    chore: use Date().getFullYear() instead of build year for copyright

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { author } from '@/data/siteMetadata';
-import { BUILD_SEMVER, BUILD_YEAR } from '@/data/buildVersion';
+import { BUILD_SEMVER } from '@/data/buildVersion';
 import SocialIcons from './SocialIcons';
 
 export default async function Footer() {
@@ -8,7 +8,7 @@ export default async function Footer() {
       <div className="mt-8 pt-8 border-t sm:border-0 mb-4 sm:mb-0 flex flex-col items-center">
         <SocialIcons />
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{`© ${BUILD_YEAR}`}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
           <div>&bull;</div>
           <div>{author}</div>
           <div>&bull;</div>
