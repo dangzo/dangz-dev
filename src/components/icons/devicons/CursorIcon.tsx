0f1fc5446d8a4a3d265cdfab3f6c8fa9f353b221
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-05-15 10:48:33 +0100

    fix: wrong tailwind class-name used, removing as unnecessary

--- src/components/icons/devicons/CursorIcon.tsx
+++ src/components/icons/devicons/CursorIcon.tsx
@@ -7,7 +7,6 @@ function CursorIcon(svgProps: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       version="1.1"
       viewBox="0 0 466.73 532.09"
-      className="[&_.st0]:background-color-[#edecec]"
       {...svgProps}
     >
       <path
