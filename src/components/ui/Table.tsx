cf8a9ef3b90630f8592236ee43d288cd48e356a5
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-06-21 22:45:21 +0200

    fix: vertical spacing of table component

--- src/components/ui/Table.tsx
+++ src/components/ui/Table.tsx
@@ -17,36 +17,34 @@ const Table = ({ value }: TableProps) => {
   const { rows } = value;
   
   return (
-    <div className="overflow-x-auto my-8">
-      <table
-        className="
-          min-w-full border-collapse border 
-          border-gray-300 bg-neutral-50/50 
-          dark:border-gray-700 dark:bg-neutral-800/10
-        "
-      >
-        <tbody>
-          {rows.map((row: TableRow, rowIndex: number) => (
-            <tr key={row._key || rowIndex}>
-              {row.cells.map((cell: string, cellIndex: number) => (
-                <td
-                  key={cellIndex}
-                  className="
-                    border border-gray-300 dark:border-gray-700
-                    text-secondary-light dark:text-secondary-dark font-body
-                    p-2! align-top [&_p]:my-0 [&_ul]:my-0 [&_ul]:pl-5 [&_ol]:my-0 [&_ol]:pl-5 [&_code]:rounded [&_code]:bg-accent-light/10 [&_code]:px-1 [&_code]:py-0.5 dark:[&_code]:bg-neutral-100/10
-                  "
-                >
-                  <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                    {cell}
-                  </ReactMarkdown>
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <table
+      className="
+        min-w-full border-collapse border mb-10
+        border-gray-300 bg-neutral-50/50 
+        dark:border-gray-700 dark:bg-neutral-800/10
+      "
+    >
+      <tbody>
+        {rows.map((row: TableRow, rowIndex: number) => (
+          <tr key={row._key || rowIndex}>
+            {row.cells.map((cell: string, cellIndex: number) => (
+              <td
+                key={cellIndex}
+                className="
+                  border border-gray-300 dark:border-gray-700
+                  text-secondary-light dark:text-secondary-dark font-body
+                  p-2! align-top [&_p]:my-0 [&_ul]:my-0 [&_ul]:pl-5 [&_ol]:my-0 [&_ol]:pl-5 [&_code]:rounded [&_code]:bg-accent-light/10 [&_code]:px-1 [&_code]:py-0.5 dark:[&_code]:bg-neutral-100/10
+                "
+              >
+                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                  {cell}
+                </ReactMarkdown>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
